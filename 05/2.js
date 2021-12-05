6127f4fdff4c86cf0e797871a45580ca1cd7e8f0
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2021-12-05 11:32:06 +0100

    day 5

---
+++ 05/2.js
@@ -0,0 +1,81 @@
+import fs from 'fs';
+
+fs.readFile('./input.txt', (err, data) => {
+	if (err) throw err;
+	const input = data.toString();
+	const lines = input
+		.split('\n')
+		.map((l) =>
+			l.split(' -> ').map((p) => p.split(',').map((n) => parseFloat(n)))
+		);
+
+	const gridSize = [0, 0];
+	for (const l of lines) {
+		gridSize[0] = Math.max(gridSize[0], l[0][1], l[1][1]);
+		gridSize[1] = Math.max(gridSize[1], l[0][0], l[1][0]);
+	}
+	const size = Math.max(gridSize[0] + 1, gridSize[1] + 1);
+
+	const grid = Array(size)
+		.fill(0)
+		.map(() =>
+			Array(size)
+				.fill(0)
+				.map(() => 0)
+		);
+
+	lines.forEach((l) => {
+		runHVDLine(grid, l);
+	});
+
+	let secondStar = 0;
+	for (const l of grid) {
+		for (const v of l) {
+			if (v >= 2) {
+				secondStar++;
+			}
+		}
+	}
+	console.log({ secondStar });
+});
+
+function isDiagonal(line) {
+	let a = Math.abs(line[1][0] - line[0][0]);
+	let b = Math.abs(line[1][1] - line[0][1]);
+	if (a > b) {
+		const temp = a;
+		a = b;
+		b = temp;
+	}
+	if (b == 0) return 1;
+	return a / b;
+}
+
+function runHVDLine(grid, line) {
+	if (isDiagonal(line)) {
+		const y1 = Math.min(line[0][1], line[1][1]);
+		const y2 = Math.max(line[0][1], line[1][1]);
+		const x1 = y1 === line[0][1] ? line[0][0] : line[1][0];
+		const x2 = y1 === line[0][1] ? line[1][0] : line[0][0];
+		const dx = x2 > x1 ? 1 : -1;
+		let x = x1;
+		for (let y = y1; y <= y2; y++) {
+			grid[y][x]++;
+			x += dx;
+		}
+	} else if (line[0][0] === line[1][0]) {
+		const x = line[0][0];
+		const y1 = Math.min(line[0][1], line[1][1]);
+		const y2 = Math.max(line[0][1], line[1][1]);
+		for (let y = y1; y <= y2; y++) {
+			grid[y][x] += 1;
+		}
+	} else if (line[0][1] === line[1][1]) {
+		const y = line[0][1];
+		const x1 = Math.min(line[0][0], line[1][0]);
+		const x2 = Math.max(line[0][0], line[1][0]);
+		for (let x = x1; x <= x2; x++) {
+			grid[y][x] += 1;
+		}
+	}
+}
