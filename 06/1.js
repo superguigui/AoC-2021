a5fbd4221ca5125b4740218ee0bc28f8f4f63b84
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2021-12-06 09:53:04 +0100

    day 6

---
+++ 06/1.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+fs.readFile('./input.txt', (err, data) => {
+	if (err) throw err;
+	const input = data
+		.toString()
+		.split(',')
+		.map((n) => parseFloat(n));
+	const counters = Array(9).fill(0);
+	input.forEach((v) => counters[v]++);
+	for (let i = 0; i < 80; i++) {
+		const babies = counters[0];
+		for (let j = 0; j < counters.length - 1; j++) {
+			counters[j] = counters[j + 1];
+		}
+		counters[counters.length - 1] = babies;
+		counters[6] += babies;
+	}
+	const firstStar = counters.reduce((a, b) => a + b, 0);
+	console.log({ firstStar });
+});
