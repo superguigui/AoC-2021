a68407a330c3a84ed088e4bd7a91c57355d70dc5
Guillaume Gouessan <guillaume.gouessan@gmail.com>
2021-12-08 22:59:50 +0100

    slightly better day 8 part 2

--- 08/2.js
+++ 08/2.js
@@ -2,43 +2,38 @@ import fs from 'fs';
 
 fs.readFile('./input.txt', (err, data) => {
 	if (err) throw err;
-	const input = data
-		.toString()
-		.split('\n')
-		.map((n) => n.split(' | ').map((b) => b.split(' ')));
-
-	const secondStar = input.reduce((a, b) => a + decode(b), 0);
-	console.log({ secondStar });
+	console.log({
+		secondStar: data
+			.toString()
+			.split('\n')
+			.map((n) => n.split(' | ').map((b) => b.split(' ')))
+			.reduce((a, b) => a + decode(b), 0),
+	});
 });
 
-const commonCharacterCount = (s1, s2) => {
-	s2 = s2.split('');
-	return s1.split('').filter((c) => s2.includes(c)).length;
-};
+const has = (a, b) => a.match(new RegExp(b));
+const common = (a, b) => a.split('').filter((c) => has(b, c)).length;
 const sortAlpha = (a) => a.split('').sort().join('');
-const findWithLength = (signals, l) => signals.find((a) => a.length === l);
-const findAllWithLength = (signals, l) => signals.filter((a) => a.length === l);
-const contains = (target, chars) =>
-	commonCharacterCount(target, chars) === chars.length;
+const contains = (a, b) => common(a, b) === b.length;
 
 function decode(line) {
-	const signals = line[0];
-	const map = {},
+	const sig = line[0],
+		map = {},
 		dic = {};
-	map[1] = findWithLength(signals, 2);
-	map[7] = findWithLength(signals, 3);
-	map[4] = findWithLength(signals, 4);
-	map[8] = findWithLength(signals, 7);
+	map[1] = sig.find((a) => a.length === 2);
+	map[7] = sig.find((a) => a.length === 3);
+	map[4] = sig.find((a) => a.length === 4);
+	map[8] = sig.find((a) => a.length === 7);
 
-	findAllWithLength(signals, 6).forEach((a) => {
+	sig.filter((a) => a.length === 6).forEach((a) => {
 		if (contains(a, map[4])) map[9] = a;
 		else if (contains(a, map[7])) map[0] = a;
 		else map[6] = a;
 	});
 
-	findAllWithLength(signals, 5).forEach((a) => {
+	sig.filter((a) => a.length === 5).forEach((a) => {
 		if (contains(a, map[7])) map[3] = a;
-		else if (commonCharacterCount(a, map[4]) === 3) map[5] = a;
+		else if (common(a, map[4]) === 3) map[5] = a;
 		else map[2] = a;
 	});
 
